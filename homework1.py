d8029da99a785dce114e531cc0d0d7e14391d938
kvakva22 <qwexykk@gmail.com>
2024-11-05 18:26:37 +0200

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -139,9 +139,10 @@ def add_contact(args, book: AddressBook):
     record = book.find(name)
     message = "Contact updated."
     if record is None:
-        record = Record(name)
-        book.add_record(record)
-        message = "Contact added."
+        if Phone(phone):
+            record = Record(name)   
+            book.add_record(record)
+            message = "Contact added."
     if phone:
         record.add_phone(phone)
     return message
